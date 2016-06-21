7f655b8f791140e80c26bf2f20fd03068fda3c15
upadv001 <469297@mnameits-MacBook-Pro-2.local>
2016-06-22 00:14:01 +0530

    Revert "Comments added as part of develop branch development"
    This reverts commit bd6bd83a69f5c47c27e5728d57bd2eb129516717.

--- src/main/java/com/sphibmvc/controller/EmployeeController.java
+++ src/main/java/com/sphibmvc/controller/EmployeeController.java
@@ -16,11 +16,9 @@ import com.sphibmvc.services.EmployeeService;
 
 //@Controller
 public class EmployeeController  {
-
-	// EmployeeService Object : added as part of develop branch 
+	
 	private EmployeeService employeeService;
 	
-	// getter method for EmployeeService Object : added as part of develop branch 
     public EmployeeService getEmployeeService() {
 		return employeeService;
 	}
